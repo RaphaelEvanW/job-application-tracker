951a4d9d7a03c54593f3874c20c891c978ca9fb9
RaphaelEvanW <raphael.wijayanto@gmail.com>
2026-01-13 21:07:42 +0700

    backend: DB connection function, user table database, and polish some of the feature

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/RaphaelEvanW/job-application-tracker/backend/internal/database"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	r := gin.Default()
+
+	database.Connect()
 
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
